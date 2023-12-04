5bede9486523565add931e6f6b13a8719a325bc7
amsmeier <amsmeier@gmail.com>
2023-12-03 20:06:46 -0500

    Update copy_triplet_data.m

--- copy_triplet_data.m
+++ copy_triplet_data.m
@@ -1,13 +1,16 @@
 % copy data from the triplet server to new drive location
 
 dbpath = 'Z:\DBS'; 
-topdir_to_copy_to = '\\tsclient\D\triplet'; 
+
+% topdir_to_copy_to = '\\tsclient\D\triplet'; 
+topdir_to_copy_to = '\\tsclient\E\triplet'; 
+% topdir_to_copy_to = 'D:\triplet';
 
  dd = struct2table(dir(dbpath));
  subs = dd.name(contains(dd.name,'DBS3')); %%% subjects from the 3000 (triplet) series
  nsubs = length(subs);
 
- for isub = 1:nsubs
+ for isub = 7:nsubs
      thissub = subs{isub}
      dir_to_copy = [dbpath filesep thissub filesep 'Preprocessed Data']; 
      new_sub_dir = [topdir_to_copy_to filesep thissub];
